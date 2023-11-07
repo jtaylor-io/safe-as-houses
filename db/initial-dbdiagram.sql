1c47bf0b630d5ad954987065de3c66f21e0cbeaf
John Taylor <johnmichaeltaylor79@gmail.com>
2023-11-07 11:50:44 +0000

    Initial dbml schema and corresponding dbdiagram.io output
    closes #1

---
+++ db/initial-dbdiagram.sql
@@ -0,0 +1,41 @@
+CREATE TABLE "accounts" (
+  "id" bigserial PRIMARY KEY,
+  "owner" varchar NOT NULL,
+  "balance" numeric NOT NULL,
+  "currency" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "entries" (
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigint NOT NULL,
+  "amount" numeric NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "transfers" (
+  "id" bigserial PRIMARY KEY,
+  "from_account_id" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
+  "amount" numeric NOT NULL
+);
+
+CREATE INDEX ON "entries" ("account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id");
+
+CREATE INDEX ON "transfers" ("to_account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
+
+COMMENT ON TABLE "accounts" IS 'Stores Bank Account Data';
+
+COMMENT ON COLUMN "entries"."amount" IS 'credit is +ve and debit is -ve';
+
+COMMENT ON COLUMN "transfers"."amount" IS 'amount must be +ve value';
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
