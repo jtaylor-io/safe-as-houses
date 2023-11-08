af5c2e6f51b1e4a5eefeb032de898c3fdf9dc073
John Taylor <johnmichaeltaylor79@gmail.com>
2023-11-08 13:17:46 +0000

    Add Entry crud tests

---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,81 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/jtaylor-io/safe-as-houses/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomEntry(t *testing.T, account Account) Entry {
+	arg := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    util.RandomMoney(),
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, arg.AccountID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+
+	require.NotZero(t, entry.ID)
+	require.WithinDuration(t, entry.CreatedAt, time.Now(), time.Second)
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	createRandomEntry(t, account)
+}
+
+func TestGetEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	entry1 := createRandomEntry(t, account)
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.Equal(t, entry1.CreatedAt, entry2.CreatedAt)
+}
+
+func TestListEntries(t *testing.T) {
+	account := createRandomAccount(t)
+	for i := 0; i < 10; i++ {
+		createRandomEntry(t, account)
+	}
+
+	arg := ListEntriesParams{
+		AccountID: account.ID,
+		Offset:    5,
+		Limit:     5,
+	}
+
+	entries, err := testQueries.ListEntries(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+		require.Equal(t, account.ID, entry.AccountID)
+	}
+}
+
+func TestDeleteEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	entry := createRandomEntry(t, account)
+	err := testQueries.DeleteEntry(context.Background(), entry.ID)
+
+	require.NoError(t, err)
+	entry1, err := testQueries.GetEntry(context.Background(), entry.ID)
+
+	require.Error(t, err)
+	require.Empty(t, entry1)
+}
