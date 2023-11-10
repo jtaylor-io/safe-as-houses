49e50425c82fd6a4c1826d8aa882db525d222a0f
John Taylor <johnmichaeltaylor79@gmail.com>
2023-11-10 23:36:25 +0000

    update create account http status codes
    - closes #17

--- api/account.go
+++ api/account.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	db "github.com/jtaylor-io/safe-as-houses/db/sqlc"
+	"github.com/lib/pq"
 	"github.com/shopspring/decimal"
 )
 
@@ -29,6 +30,13 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
