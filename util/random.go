4e976734fd0441df645a25072a3e5cb567f6a77e
John Taylor <johnmichaeltaylor79@gmail.com>
2023-11-10 23:22:19 +0000

    Add user db queries
    - closes #16

--- util/random.go
+++ util/random.go
@@ -50,3 +50,8 @@ func RandomCurrency() string {
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
+
+// RandomEmail generates a random email address
+func RandomEmail() string {
+	return RandomString(10) + "@example.com"
+}
