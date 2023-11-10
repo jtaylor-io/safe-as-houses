193c86796b67fe051fd430b4dfa71159f521d3ad
John Taylor <johnmichaeltaylor79@gmail.com>
2023-11-10 22:08:56 +0000

    Add missing index

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -21,6 +21,8 @@ CREATE TABLE "transfers" (
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
+CREATE INDEX ON "accounts" ("owner");
+
 CREATE INDEX ON "entries" ("account_id");
 
 CREATE INDEX ON "transfers" ("from_account_id");
