1ea5ca585f9f192afec8c2c9378ff58b9e44fa94
John Taylor <johnmichaeltaylor79@gmail.com>
2023-11-23 16:11:34 +0000

    Structured logs for http endpoints
    - closes #51

--- gapi/logger.go
+++ gapi/logger.go
@@ -2,6 +2,7 @@ package gapi
 
 import (
 	"context"
+	"net/http"
 	"time"
 
 	"github.com/rs/zerolog/log"
@@ -18,6 +19,7 @@ func GrpcLogger(
 ) (resp any, err error) {
 	startTime := time.Now()
 	result, err := handler(ctx, req)
+	duration := time.Since(startTime)
 
 	statusCode := codes.Unknown
 	if st, ok := status.FromError(err); ok {
@@ -33,7 +35,49 @@ func GrpcLogger(
 		Str("method", info.FullMethod).
 		Int("status_code", int(statusCode)).
 		Str("status_text", statusCode.String()).
-		Dur("duration", time.Since(startTime)).
+		Dur("duration", duration).
 		Msg("received grpc request")
 	return result, err
+}
+
+type ResponseRecorder struct {
+	http.ResponseWriter
+	StatusCode int
+	Body       []byte
+}
+
+func (rec *ResponseRecorder) WriteHeader(statusCode int) {
+	rec.StatusCode = statusCode
+	rec.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (rec *ResponseRecorder) Write(body []byte) (int, error) {
+	rec.Body = body
+	return rec.ResponseWriter.Write(body)
+}
+
+func HttpLogger(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		startTime := time.Now()
+		rec := &ResponseRecorder{
+			ResponseWriter: res,
+			StatusCode:     http.StatusOK,
+		}
+
+		handler.ServeHTTP(rec, req)
+		duration := time.Since(startTime)
+
+		logger := log.Info()
+		if rec.StatusCode != http.StatusOK {
+			logger = log.Error().Bytes("body", rec.Body)
+		}
+
+		logger.Str("protocol", "http").
+			Str("method", req.Method).
+			Str("path", req.RequestURI).
+			Int("status_code", rec.StatusCode).
+			Str("status_text", http.StatusText(rec.StatusCode)).
+			Dur("duration", duration).
+			Msg("received http request")
+	})
 }
