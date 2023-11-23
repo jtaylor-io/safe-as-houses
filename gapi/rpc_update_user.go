332e0b1915f4d07272234f1b9cf6d2b10c28ed16
John Taylor <johnmichaeltaylor79@gmail.com>
2023-11-23 10:11:23 +0000

    Add authentication to UpdateUser endpoint
    - closes #48

--- gapi/rpc_update_user.go
+++ gapi/rpc_update_user.go
@@ -19,9 +19,18 @@ func (server *Server) UpdateUser(
 	ctx context.Context,
 	req *pb.UpdateUserRequest,
 ) (*pb.UpdateUserResponse, error) {
+	authPayload, err := server.authorizeUser(ctx)
+	if err != nil {
+		return nil, unauthenticatedError(err)
+	}
+
 	violations := validateUpdateUserRequest(req)
 	if violations != nil {
 		return nil, invalidArgumentError(violations)
+	}
+
+	if authPayload.Username != req.GetUsername() {
+		return nil, status.Errorf(codes.PermissionDenied, "cannot update another user")
 	}
 
 	arg := db.UpdateUserParams{
