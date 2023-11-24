ae2ca83c5704bb4932cf28ed9afb4205f4957484
John Taylor <johnmichaeltaylor79@gmail.com>
2023-11-24 14:57:16 +0000

    Initial wiring of async woker with web server

--- Makefile
+++ Makefile
@@ -46,5 +46,11 @@ proto:
 evans:
 	evans --host localhost --port 9090 -r repl
 
-.PHONY: postgres createdb dropdb migrateup migratedown smigrateup1 migratedown1 sqlc test server mock proto
+redis:
+	docker run --name redis -p 6379:6379 -d redis:7.2-alpine
+
+redis_ping:
+	docker exec -it redis redis-cli ping
+
+.PHONY: postgres createdb dropdb migrateup migratedown smigrateup1 migratedown1 sqlc test server mock proto redis redis_ping
 
--- gapi/server.go
+++ gapi/server.go
@@ -7,27 +7,34 @@ import (
 	"github.com/jtaylor-io/safe-as-houses/pb"
 	"github.com/jtaylor-io/safe-as-houses/token"
 	"github.com/jtaylor-io/safe-as-houses/util"
+	"github.com/jtaylor-io/safe-as-houses/worker"
 )
 
 // Server serves gRPC requests for banking service
 type Server struct {
 	pb.UnimplementedSafeAsHousesServer
-	config     util.Config
-	store      db.Store
-	tokenMaker token.Maker
+	config          util.Config
+	store           db.Store
+	tokenMaker      token.Maker
+	taskDistributor worker.TaskDistributor
 }
 
 // NewServer creates a new gRPC server
-func NewServer(config util.Config, store db.Store) (*Server, error) {
+func NewServer(
+	config util.Config,
+	store db.Store,
+	taskDistributor worker.TaskDistributor,
+) (*Server, error) {
 	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
 	if err != nil {
 		return nil, fmt.Errorf("cannot create token maker: %w", err)
 	}
 
 	server := &Server{
-		config:     config,
-		store:      store,
-		tokenMaker: tokenMaker,
+		config:          config,
+		store:           store,
+		tokenMaker:      tokenMaker,
+		taskDistributor: taskDistributor,
 	}
 
 	return server, nil
