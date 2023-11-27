67667568dd8f661c1715151f7e4315a50bd2dd6d
John Taylor <johnmichaeltaylor79@gmail.com>
2023-11-27 09:53:37 +0000

    Add custom logger for asynq and redis
    - closes #53

--- worker/processor.go
+++ worker/processor.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/hibiken/asynq"
 	db "github.com/jtaylor-io/safe-as-houses/db/sqlc"
+	"github.com/redis/go-redis/v9"
+	"github.com/rs/zerolog/log"
 )
 
 const (
@@ -23,6 +25,8 @@ type RedisTaskProcessor struct {
 }
 
 func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskProcessor {
+	logger := NewLogger()
+	redis.SetLogger(logger)
 	server := asynq.NewServer(
 		redisOpt,
 		asynq.Config{
@@ -30,6 +34,16 @@ func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskPr
 				QueueCritical: 10,
 				QueueDefault:  5,
 			},
+			ErrorHandler: asynq.ErrorHandlerFunc(
+				func(ctx context.Context, task *asynq.Task, err error) {
+					log.Error().
+						Err(err).
+						Str("type", task.Type()).
+						Bytes("payload", task.Payload()).
+						Msg("prosess task failed")
+				},
+			),
+			Logger: logger,
 		},
 	)
 	return &RedisTaskProcessor{
