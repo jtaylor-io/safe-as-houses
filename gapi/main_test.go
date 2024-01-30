c2e3275e14e1a40dec94036b6cda7bfe6cd93aff
John Taylor <johnmichaeltaylor79@gmail.com>
2024-01-30 17:03:27 +0000

    Initial rpc_update_user tests

--- gapi/main_test.go
+++ gapi/main_test.go
@@ -1,13 +1,17 @@
 package gapi
 
 import (
+	"context"
+	"fmt"
 	"testing"
 	"time"
 
 	db "github.com/jtaylor-io/safe-as-houses/db/sqlc"
+	"github.com/jtaylor-io/safe-as-houses/token"
 	"github.com/jtaylor-io/safe-as-houses/util"
 	"github.com/jtaylor-io/safe-as-houses/worker"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/metadata"
 )
 
 func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDistributor) *Server {
@@ -20,4 +24,22 @@ func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDist
 	require.NoError(t, err)
 
 	return server
+}
+
+func newContextWithBearerToken(
+	t *testing.T,
+	tokenMaker token.Maker,
+	username string,
+	duration time.Duration,
+) context.Context {
+	accessToken, _, err := tokenMaker.CreateToken(username, duration)
+	require.NoError(t, err)
+	bearerToken := fmt.Sprintf("%s %s", authorizationTypeBearer, accessToken)
+
+	md := metadata.MD{
+		authorizationHeader: []string{
+			bearerToken,
+		},
+	}
+	return metadata.NewIncomingContext(context.Background(), md)
 }
