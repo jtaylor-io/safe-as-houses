45e7c8c6b5dc3b23a5f6fc25302d3ed31831fbb0
John Taylor <johnmichaeltaylor79@gmail.com>
2024-01-30 23:13:12 +0000

    Initial RBAC implementation

--- api/user_test.go
+++ api/user_test.go
@@ -379,6 +379,7 @@ func randomUser(t *testing.T) (db.User, string) {
 
 	return db.User{
 		Username:       util.RandomOwner(),
+		Role:           util.DepositorRole,
 		HashedPassword: hashedPassword,
 		FullName:       util.RandomOwner(),
 		Email:          util.RandomEmail(),
